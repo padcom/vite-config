d3e313a50321efeee086dd59237d038149b67575
Matthias Hryniszak <padcom@gmail.com>
2024-10-24 14:44:31 +0200

    Added configuration option to vue lib config so devtools options can be specified

--- packages/vite-config-vue-lib/vite-vue-lib.config.js
+++ packages/vite-config-vue-lib/vite-vue-lib.config.js
@@ -2,8 +2,14 @@ import { defineLibConfig } from '@padcom/vite-config-lib'
 import { defineVueBaseConfig, defineVueTestConfig } from '@padcom/vite-config-vue'
 
 /**
+ * @typedef {import('vite').UserConfig} UserConfig
+ * @typedef {import('vite-plugin-vue-devtools').VitePluginVueDevToolsOptions} DevToolsOptions
+ *
+ * @typedef {Object} DevTools
+ * @property {DevToolsOptions} devtools
+ *
  * @param {import('@padcom/vite-config-default').PackageJSON} pkg
- * @param {import('vite').UserConfig} overrides
+ * @param {UserConfig & DevTools} overrides
  */
 export function defineVueLibConfig(pkg, overrides) {
   return defineLibConfig(pkg, defineVueBaseConfig(defineVueTestConfig(overrides)))
