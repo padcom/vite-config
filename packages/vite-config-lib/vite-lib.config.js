e3f48d5dc46f19f34b4ba633cb985e484c9e52c4
Matthias Hryniszak <padcom@gmail.com>
2024-11-15 17:00:11 +0100

    Changed the way the library output is generated to not include library name but be just index

--- packages/vite-config-lib/vite-lib.config.js
+++ packages/vite-config-lib/vite-lib.config.js
@@ -22,6 +22,7 @@ export function defineBaseLibConfig(pkg, overrides = {}) {
       lib: {
         entry: './src/index.ts',
         name: pkg.name.split('/').at(-1),
+        fileName: 'index',
       },
       sourcemap: true,
     },
